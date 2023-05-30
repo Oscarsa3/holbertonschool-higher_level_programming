e9d27980b2379e54daa9efb6753964655fe7cdeb
“Oscarsa3” <oscarorlandosalinassilvestre@gmail.com>
2023-05-30 13:10:25 -0700

    feat: add program that prints the result of the addition of all arguments

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    total = 0
+    if len(sys.argv) > 1:
+        for i in range(len(sys.argv) - 1):
+            total += int(sys.argv[i + 1])
+            i += 1
+    print("{}".format(total))

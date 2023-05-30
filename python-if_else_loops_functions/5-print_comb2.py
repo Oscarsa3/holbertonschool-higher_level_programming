f6b33f0afdcffe9b6c58b0d9b0b8fe70557feed8
“Oscarsa3” <oscarorlandosalinassilvestre@gmail.com>
2023-05-29 17:32:37 -0700

    feat: add program that prints numbers from 0 to 99

---
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(0, 100):
+    if i < 10:
+        print("0{}, ".format(i), end='')
+    elif i == 99:
+        print("{}".format(i))
+    else:
+        print("{}, ".format(i), end='')

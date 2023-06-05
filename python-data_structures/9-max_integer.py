6560ba53711e56024c38347a48d6a56d3706a1ed
“Oscarsa3” <oscarorlandosalinassilvestre@gmail.com>
2023-06-05 10:23:17 -0700

    fix: pycodestyle

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list != None:
+    if my_list:
         ma = my_list[0]
         for i in range(len(my_list) - 1):
             if ma < my_list[i]:
                 ma = my_list[i]
         return ma
-    return None
+    return "None"

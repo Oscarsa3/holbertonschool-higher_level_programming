85a2286576ea22dba7e65f793b18e1b56e92c88b
“Oscarsa3” <oscarorlandosalinassilvestre@gmail.com>
2023-06-05 10:56:59 -0700

    fix: output

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,9 +2,12 @@
 def divisible_by_2(my_list=[]):
     if my_list:
         new = my_list.copy()
-        for i in range(len(my_list) - 1):
+        i = 0
+        while i < len(my_list):
             if my_list[i] % 2 == 0:
                 new[i] = True
             else:
                 new[i] = False
+            i += 1
+        print(new)
         return new

0f120089690f5a30cd36214e518f66482eda58f1
“Oscarsa3” <oscarorlandosalinassilvestre@gmail.com>
2023-06-06 14:17:22 -0700

    feat: add lambda in this function

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     new = [[1]] * len(matrix)
-    row = 0
-    while row < len(matrix):
+    for row in range(len(matrix)):
         a = row
-        i = 0
-        while i < len(matrix[row]):
-            new[a] = list(map(potencia, matrix[row]))
-            i += 1
-        row += 1
+        for i in range(len(matrix[row])):
+            new[a] = list(map(lambda x: x ** 2, matrix[row]))
     return new
-
-
-def potencia(numero):
-    return numero * numero

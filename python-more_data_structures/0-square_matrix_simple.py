345585c6fc60d4aae24e127b40d9bcf469b61482
“Oscarsa3” <oscarorlandosalinassilvestre@gmail.com>
2023-06-06 12:52:25 -0700

    feat: add function that computes the square value of all integers of a matrix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new = [[1]] * len(matrix)
-    row = 0
-    while row < len(matrix):
-        a = row
-        i = 0
-        while i < len(matrix[row]):
-            new[a] = list(map(potencia, matrix[row]))
-            i += 1
-        row += 1
-    return new
+    if matrix:
+        new = [[1]] * len(matrix)
+        row = 0
+        while row < len(matrix):
+            a = row
+            i = 0
+            while i < len(matrix[row]):
+                new[a] = list(map(potencia, matrix[row]))
+                i += 1
+            row += 1
+        return new
 
 
 def potencia(numero):

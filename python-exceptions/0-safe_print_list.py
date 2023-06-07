1ff4918ca4bf89ce139781dd759c3a83d6f5704d
“Oscarsa3” <oscarorlandosalinassilvestre@gmail.com>
2023-06-07 10:38:29 -0700

    fix: add other excepts

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    i = 0
     try:
-        for i in range(x):
-            print(my_list[i], end="")
-        print()
-        return i + 1
-    except Xerror:
+        if my_list:
+            for i in range(x):
+                print(my_list[i], end="")
+            print()
+            return i + 1
+    except NameError:
         print()
         return i
+    except Error:
+        print()
+        return 0

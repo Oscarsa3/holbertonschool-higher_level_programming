b2d00ad4f5d8f63afb297b4cae03fe448c9962e3
“Oscarsa3” <oscarorlandosalinassilvestre@gmail.com>
2023-06-07 10:05:13 -0700

    fix: other loop

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        i = 0
-        while i < x:
+        for i in range(x):
             print(my_list[i], end="")
-            i += 1
         print()
         return i + 1
     except Xerror:

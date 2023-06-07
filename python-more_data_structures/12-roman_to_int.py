e13109c87bfd1a42f38c5d15bf6c1895587ed242
“Oscarsa3” <oscarorlandosalinassilvestre@gmail.com>
2023-06-06 18:15:00 -0700

    feat: add function that converts a Roman numeral to an integer.

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+    romanos = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    if roman_string in romanos.keys():
+        return romanos.get(roman_string)
+    else:
+        b = 0
+        for i in range(len(roman_string)):
+            a = roman_string[i]
+            if i == len(roman_string) - 1:
+                b += romanos.get(a)
+                break
+            elif roman_string[i] == 'I' and roman_string[i + 1] == 'V':
+                b += romanos.get(roman_string[i + 1]) - romanos.get(a)
+                break
+            elif roman_string[i] == 'I' and roman_string[i + 1] == 'X':
+                b += romanos.get(roman_string[i + 1]) - romanos.get(a)
+                break
+            elif roman_string[i] == 'I' and roman_string[i + 1] == 'L':
+                b += romanos.get(roman_string[i + 1]) - romanos.get(a)
+                break
+            elif roman_string[i] == 'I' and roman_string[i + 1] == 'C':
+                b += romanos.get(roman_string[i + 1]) - romanos.get(a)
+                break
+            elif roman_string[i] == 'I' and roman_string[i + 1] == 'D':
+                b += romanos.get(roman_string[i + 1]) - romanos.get(a)
+                break
+            elif roman_string[i] == 'I' and roman_string[i + 1] == 'M':
+                b += romanos.get(roman_string[i + 1]) - romanos.get(a)
+                break
+            b += romanos.get(a)
+        return abs(b)

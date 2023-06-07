42c73be029d840763678bed20b27cd9cfd3bf0f3
“Oscarsa3” <oscarorlandosalinassilvestre@gmail.com>
2023-06-07 10:40:13 -0700

    fix: errors with output

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    i = 0
     try:
-        if my_list:
-            for i in range(x):
-                print(my_list[i], end="")
-            print()
-            return i + 1
+        for i in range(x):
+            print(my_list[i], end="")
+        print()
+        return i + 1
     except NameError:
         print()
         return i
-    except Error:
-        print()
-        return 0

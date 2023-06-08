b6fc2fff75e3752d142ff62ad48ebcf114663212
“Oscarsa3” <oscarorlandosalinassilvestre@gmail.com>
2023-06-07 18:03:39 -0700

    fix: name of except

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i], end="")
             contador += 1
-        except NameError:
+        except:
             continue
     print()
     return contador

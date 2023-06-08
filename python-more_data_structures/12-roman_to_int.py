8ab6f416658707060ce13aa1d83767273c7f7356
“Oscarsa3” <oscarorlandosalinassilvestre@gmail.com>
2023-06-08 13:52:51 -0700

    fix: check if the argument is string

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string:
+    if not isinstance(roman_string, str) or not roman_string:
         return 0
     roman_string = roman_string.upper()
     romanos = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
@@ -9,18 +9,15 @@ def roman_to_int(roman_string):
     else:
         b = 0
         for i in range(len(roman_string)):
-            a = roman_string[i]
             if i == len(roman_string) - 1:
-                b += romanos.get(a)
+                b += romanos.get(roman_string[i])
                 continue
             if roman_string[i] == 'I' and roman_string[i + 1] == 'V':
-                b += romanos.get(roman_string[i + 1]) - romanos.get(a)
+                b += romanos.get(roman_string[i + 1]) - romanos.get(roman_string[i])
                 break
             elif roman_string[i] == 'I' and roman_string[i + 1] == 'X':
-                b += romanos.get(roman_string[i + 1]) - romanos.get(a)
+                b += romanos.get(roman_string[i + 1]) - romanos.get(roman_string[i])
                 break
             else:
-                b += romanos.get(a)
-        if b >= 3999:
-            return 3999
+                b += romanos.get(roman_string[i])
     return b

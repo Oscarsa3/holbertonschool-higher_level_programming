0705846fbdae20a9909619ef3603dfc6dc3cf74f
“Oscarsa3” <oscarorlandosalinassilvestre@gmail.com>
2023-06-08 13:55:45 -0700

    fix: check if the argument is None

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or not roman_string:
+    if not isinstance(roman_string, str) or roman_string == None:
+        return 0
+    if not roman_string:
         return 0
     roman_string = roman_string.upper()
     romanos = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}

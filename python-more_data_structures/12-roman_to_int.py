244ff90321d7fd6ac0f72969b161c956c8109cc6
“Oscarsa3” <oscarorlandosalinassilvestre@gmail.com>
2023-06-08 14:02:28 -0700

    fix: style error

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string == None:
+    if not isinstance(roman_string, str):
         return 0
     if not roman_string:
         return 0
@@ -11,15 +11,16 @@ def roman_to_int(roman_string):
     else:
         b = 0
         for i in range(len(roman_string)):
+            a = roman_string[i]
             if i == len(roman_string) - 1:
                 b += romanos.get(roman_string[i])
                 continue
             if roman_string[i] == 'I' and roman_string[i + 1] == 'V':
-                b += romanos.get(roman_string[i + 1]) - romanos.get(roman_string[i])
+                b += romanos.get(roman_string[i + 1]) - romanos.get(a)
                 break
             elif roman_string[i] == 'I' and roman_string[i + 1] == 'X':
-                b += romanos.get(roman_string[i + 1]) - romanos.get(roman_string[i])
+                b += romanos.get(roman_string[i + 1]) - romanos.get(a)
                 break
             else:
-                b += romanos.get(roman_string[i])
+                b += romanos.get(a)
     return b

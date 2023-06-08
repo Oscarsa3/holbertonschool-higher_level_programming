69d42dbe631210734a7b4b62832d323fa696e6dc
“Oscarsa3” <oscarorlandosalinassilvestre@gmail.com>
2023-06-08 15:40:58 -0700

    fix: output errors

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -4,28 +4,26 @@ def roman_to_int(roman_string):
         return 0
     if not roman_string:
         return 0
-    romanos = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    ro = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    rom = {'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
+    romanos = {**ro, **rom}
     if roman_string in romanos.keys():
         return romanos.get(roman_string)
     else:
         b = 0
-        for i in range(len(roman_string)):
+        i = 0
+        while i < len(roman_string):
             a = roman_string[i]
             if i == len(roman_string) - 1:
                 b += romanos.get(a)
-                break
-            if a == 'I' and roman_string[i + 1] == 'V':
-                if roman_string[i - 1] != 'I':
-                    b += romanos.get(roman_string[i + 1]) - romanos.get(a)
-                    break
+            elif roman_string[i: i + 2] in romanos.keys():
+                a = roman_string[i: i + 2]
                 b += romanos.get(a)
-            elif a == 'I' and roman_string[i + 1] == 'X':
-                if roman_string[i - 1] != 'I':
-                    b += romanos.get(roman_string[i + 1]) - romanos.get(a)
-                    break
-                b += romanos.get(a)
+                a = roman_string[i]
+                i += 1
             elif not roman_string[i] in romanos.keys():
                 return
             else:
                 b += romanos.get(a)
+            i += 1
         return b

5d8012d3d310392c106f26af5754eb7228166057
“Oscarsa3” <oscarorlandosalinassilvestre@gmail.com>
2023-06-13 15:14:18 -0700

    fix: documentation

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""Create function than adds 2 integers"""
+
+
 def add_integer(a, b=98):
+    """Return add integer"""
+
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:

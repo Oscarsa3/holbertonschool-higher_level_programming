b6c97422c23bfd1548b6e9f495a204d3ce45dff2
“Oscarsa3” <oscarorlandosalinassilvestre@gmail.com>
2023-06-14 13:59:27 -0700

    fix: errors

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -15,6 +15,4 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    a = int(a)
-    b = int(b)
-    return a + b
+    return int(a) + int(b)

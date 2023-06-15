b0c6102287a84e3550fc001644ec67c157e275d9
“Oscarsa3” <oscarorlandosalinassilvestre@gmail.com>
2023-06-15 11:19:35 -0700

    fix: documentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""Create the function that prints a text with 2 new lines
-after each of these characters: ., ? and :"""
+"""Create the function that prints
+a text with 2 new lines
+after each of these
+characters: ., ? and :"""
 
 
 def text_indentation(text):
-    """Define the function"""
+    """Define
+the
+function"""
 
     if type(text) != str:
         raise TypeError("text must be a string")

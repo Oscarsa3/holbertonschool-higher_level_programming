74dd683b9bb388f8e9936065f2b2e13c390b4d28
“Oscarsa3” <oscarorlandosalinassilvestre@gmail.com>
2023-06-15 13:56:25 -0700

    feat: add a class Rectangle that defines a rectangle

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Create an empty class Rectangle that defines a rectangle"""
+
+
+class Rectangle:
+    """Define the function"""
+    def __init__(self, width=0, height=0):
+        self.__height = height
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value

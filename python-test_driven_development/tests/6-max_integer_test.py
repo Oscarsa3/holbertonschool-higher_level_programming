3998972c2aa94328202d1cb81b9748f5db3f4405
“Oscarsa3” <oscarorlandosalinassilvestre@gmail.com>
2023-06-17 16:46:52 -0700

    fix: change assert

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])
+"""Unittest for max_integer([..])"
 """
 import unittest
 max_integer = __import__('6-max_integer').max_integer
@@ -11,14 +11,14 @@ class TestMaxInteger(unittest.TestCase):
     def test_max(self):
         """Verificamos si los resultados son precisios o correstos"""
 
-        self.assertEqual(max_integer([1]), 1)
-        self.assertEqual(max_integer([3, 3, 3, 3, 3, 3, 3, 3, 3, 3]), 3)
-        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
-        self.assertEqual(max_integer([1, 576, -56, -5678898765]), 576)
-        self.assertEqual(max_integer([-4, -8, -2354, -1]), -1)
-        self.assertEqual(max_integer([23, 65, 789]), 789)
-        self.assertEqual(max_integer([0, 4565, 34, 5, 876]), 4565)
-        self.assertEqual(max_integer([]), None)
+        self.assertAlmostEqual(max_integer([1]), 1)
+        self.assertAlmostEqual(max_integer([3, 3, 3, 3, 3, 3, 3, 3, 3, 3]), 3)
+        self.assertAlmostEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertAlmostEqual(max_integer([1, 576, -56, -5678898765]), 576)
+        self.assertAlmostEqual(max_integer([-4, -8, -2354, -1]), -1)
+        self.assertAlmostEqual(max_integer([23, 65, 789]), 789)
+        self.assertAlmostEqual(max_integer([0, 4565, 34, 5, 876]), 4565)
+        self.assertAlmostEqual(max_integer([]), None)
 
     def test_none(self):
         """Verificamos cuando nuestra funcion está vacía"""

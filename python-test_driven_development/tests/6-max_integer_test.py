5c7b2ef75be11bf140e73cf51e2655f082f440aa
“Oscarsa3” <oscarorlandosalinassilvestre@gmail.com>
2023-06-16 20:28:14 -0700

    fix: add test cases

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -10,15 +10,17 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_max(self):
         self.assertAlmostEqual(max_integer([1, 2, 3, 4]), 4)
-
-    def test_igual(self):
-        self.assertEqual(max_integer([4, 4, 4, 4]), 4)
+        self.assertAlmostEqual(max_integer([23, 65, 789]), 789)
+        self.assertAlmostEqual(max_integer([0, 4565, 34, 5, 876]), 4565)
 
     def test_true(self):
         self.assertTrue(max_integer([2, 5, 6]))
 
     def test_none(self):
         self.assertIsNone(max_integer())
+
+    def test_notnone(self):
+        self.assertAlmostEqual(max_integer([]), None)
 
     def test_AorB(self):
         self.assertNotIn(max_integer([2, 5, 6, 1]), [4, 8, 9])

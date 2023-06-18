f748f56de8a6cc23b170c22e4c899214304615d2
“Oscarsa3” <oscarorlandosalinassilvestre@gmail.com>
2023-06-17 17:36:45 -0700

    fix: change form

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -4,26 +4,21 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
-
 class TestMaxInteger(unittest.TestCase):
-    """Define test about max_integer() function"""
+	"""Create test of max-integer() function"""
 
-    def test_max(self):
-        """Verificamos si los resultados son precisios o correstos"""
-        # Iniciamos los test
-        self.assertAlmostEqual(max_integer([-1]), -1)
-        self.assertAlmostEqual(max_integer([1]), 1)
-        self.assertAlmostEqual(max_integer([3, 3, 3, 3, 3, 3]), 3)
-        self.assertAlmostEqual(max_integer([1, 2, 3, 4]), 4)
-        self.assertAlmostEqual(max_integer([1, 576, -56, -56765]), 576)
-        self.assertAlmostEqual(max_integer([-4, -8, -2354, -1]), -1)
-        self.assertAlmostEqual(max_integer([]), None)
-        self.assertAlmostEqual(max_integer(), None)
+	def test_max(self):
+		"""define test"""
+		self.assertAlmostEqual(max_integer([3, 5, 8, 9, 2]), 9)
+		self.assertAlmostEqual(max_integer([-3, -54678, -2, -456, -22]), -2)
+		self.assertAlmostEqual(max_integer([43, -678, 782, 56, -22]), 782)
+		self.assertAlmostEqual(max_integer([-3]), -3)
+		self.assertAlmostEqual(max_integer([5]), 5)
+		self.assertAlmostEqual(max_integer([]), None)
+		self.assertAlmostEqual(max_integer(), None)
 
-    def test_values(self):
-        """Verificamos nuestra funcion"""
-        self.assertRaises(TypeError, max_integer, None)
-        self.assertRaises(Exception, max_integer("Hola"))
-        self.assertRaises(Exception, max_integer((3, 6, 8)))
-        self.assertRaises(Exception, max_integer, ["Hola", None, 8, 9])
-        self.assertRaises(Exception, max_integer, [{4: 6}, (3, 6), 7])
+	def test_values(self):
+		"""define test cases"""
+		self.assertRaises(TypeError, max_integer("Hola"))
+		self.assertRaises(Exception, max_integer((3, 8, 16, 78, 4)))
+		self.assertRaises(TypeError, max_integer, [{3: 8}, (4, 7), 9])

31a4ff0f502bf0ad651d717ea461f001a958b10c
“Oscarsa3” <oscarorlandosalinassilvestre@gmail.com>
2023-06-17 17:43:01 -0700

    fix: documentation

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -9,7 +9,7 @@ class TestMaxInteger(unittest.TestCase):
     """Create test of max-integer() function"""
 
     def test_max(self):
-        """define test"""
+        """Define test"""
         self.assertAlmostEqual(max_integer([3, 5, 8, 9, 2]), 9)
         self.assertAlmostEqual(max_integer([-3, -54678, -2, -456, -22]), -2)
         self.assertAlmostEqual(max_integer([43, -678, 782, 56, -22]), 782)
@@ -19,7 +19,7 @@ class TestMaxInteger(unittest.TestCase):
         self.assertAlmostEqual(max_integer(), None)
 
     def test_values(self):
-        """define test cases"""
+        """Define test cases"""
         self.assertRaises(TypeError, max_integer("Hola"))
         self.assertRaises(Exception, max_integer((3, 8, 16, 78, 4)))
         self.assertRaises(TypeError, max_integer, [{3: 8}, (4, 7), 9])

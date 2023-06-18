753048eaae404a4fd7413d9a926bc5a7c320cd94
“Oscarsa3” <oscarorlandosalinassilvestre@gmail.com>
2023-06-17 17:06:46 -0700

    fix: delete some cases

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -10,19 +10,20 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_max(self):
         """Verificamos si los resultados son precisios o correstos"""
+        # Iniciamos los test
         self.assertAlmostEqual(max_integer([-1]), -1)
         self.assertAlmostEqual(max_integer([1]), 1)
-        self.assertAlmostEqual(max_integer([3, 3, 3, 3, 3, 3, 3, 3, 3, 3]), 3)
+        self.assertAlmostEqual(max_integer([3, 3, 3, 3, 3, 3]), 3)
         self.assertAlmostEqual(max_integer([1, 2, 3, 4]), 4)
-        self.assertAlmostEqual(max_integer([1, 576, -56, -5678898765]), 576)
+        self.assertAlmostEqual(max_integer([1, 576, -56, -56765]), 576)
         self.assertAlmostEqual(max_integer([-4, -8, -2354, -1]), -1)
         self.assertAlmostEqual(max_integer([]), None)
         self.assertAlmostEqual(max_integer(), None)
 
-    def test_except(self):
+    def test_values(self):
         """Verificamos nuestra funcion"""
         self.assertRaises(TypeError, max_integer, None)
         self.assertRaises(Exception, max_integer("Hola"))
         self.assertRaises(Exception, max_integer((3, 6, 8)))
-        self.assertRaises(Exception, max_integer, ["Hola", 0, 8, 9])
+        self.assertRaises(Exception, max_integer, ["Hola", None, 8, 9])
         self.assertRaises(Exception, max_integer, [8, {4: 6}, (3, 6)])

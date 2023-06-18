7262623a7df238a7963c654c8eb8c853a7333f0d
“Oscarsa3” <oscarorlandosalinassilvestre@gmail.com>
2023-06-17 17:54:35 -0700

    fix: add more cases

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -9,17 +9,18 @@ class TestMaxInteger(unittest.TestCase):
     """Create test of max-integer() function"""
 
     def test_max(self):
-        """Define test"""
+        """Define some cases test"""
         self.assertAlmostEqual(max_integer([3, 5, 8, 9, 2]), 9)
         self.assertAlmostEqual(max_integer([-3, -54678, -2, -456, -22]), -2)
         self.assertAlmostEqual(max_integer([43, -678, 782, 56, -22]), 782)
+        self.assertAlmostEqual(max_integer([8, 8, 8, 8, 8]), 8)
         self.assertAlmostEqual(max_integer([-3]), -3)
         self.assertAlmostEqual(max_integer([5]), 5)
         self.assertAlmostEqual(max_integer([]), None)
         self.assertAlmostEqual(max_integer(), None)
 
     def test_values(self):
-        """Define test cases"""
+        """Define some cases test"""
         self.assertRaises(Exception, max_integer("Hola"))
         self.assertRaises(TypeError, max_integer, None)
         self.assertRaises(Exception, max_integer((3, 8, 16, 78, 4)))

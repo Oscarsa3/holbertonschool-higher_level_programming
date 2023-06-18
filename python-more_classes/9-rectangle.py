d1abbb044c7987a2412861a278abbe961163017b
“Oscarsa3” <oscarorlandosalinassilvestre@gmail.com>
2023-06-18 16:04:50 -0700

    fix: change isinstance to type

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -29,9 +29,9 @@ class Rectangle:
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """Define a static method"""
-        if not isinstance(rect_1, Rectangle):
+        if type(rect_1) != Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
-        elif not isinstance(rect_2, Rectangle):
+        elif type(rect_2) != Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
         elif rect_1.area() >= rect_2.area():
             return rect_1

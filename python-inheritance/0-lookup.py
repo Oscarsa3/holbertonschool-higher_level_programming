f35da2ce845b89c43d17c2c8d329e691f64445ff
Oscarsa3 <oscarorlandosalinassilvestre@gmail.com>
2023-06-20 10:39:49 -0700

    fix: update

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,14 +1,13 @@
 #!usr/bin/python3
 """
-function that returns
-the list attributes
+function that returns the list attributes
 and methods of object
 """
 
 
 def lookup(obj):
     """Return a list of attributes and methods od obj
-    
+
     args:
         obj: thefirsargument
 

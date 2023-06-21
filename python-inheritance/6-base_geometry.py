24255157e94321d2a0dc5f9e5779a2858ecc5618
Oscarsa3 <oscarorlandosalinassilvestre@gmail.com>
2023-06-20 21:19:26 -0700

    feat: add a method

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""an empty class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Has a method"""
+    def area(self):
+        """Return Exception"""
+        raise Exception("area() is not implemented")

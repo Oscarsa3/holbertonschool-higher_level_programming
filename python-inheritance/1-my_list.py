531cdf6665bf3dce44eabecb98814cab4834bc3f
Oscarsa3 <oscarorlandosalinassilvestre@gmail.com>
2023-06-21 00:25:00 -0700

    fix: delete some documentation

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,8 +6,5 @@ list but sorted"""
 
 class MyList(list):
     """This class inherits from list, have a method"""
-
     def print_sorted(self):
-        """Return the list but sorted"""
-
         print(sorted(self))

57476bfbd3539013fe83765f555425cd69868186
Oscarsa3 <oscarorlandosalinassilvestre@gmail.com>
2023-06-21 00:04:36 -0700

    fix: documentation

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -12,7 +12,7 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """Size must be private and positive integer"""
-        super().integer_validator("size", size)
+        self.integer_validator("size", size)
         self.__size = size
 
     def area(self):

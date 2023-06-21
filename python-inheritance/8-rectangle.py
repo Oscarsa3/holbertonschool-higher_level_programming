c7cbba1d10a6f53b892c0b092e8b7d599567fd98
Oscarsa3 <oscarorlandosalinassilvestre@gmail.com>
2023-06-20 22:45:39 -0700

    fix: call super()

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -11,5 +11,5 @@ class Rectangle(BaseGeometry):
 
         self.__width = width
         self.__height = height
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)

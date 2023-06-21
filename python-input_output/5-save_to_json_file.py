51e4b3678c6e69dfa5da47cda268ec70cd3c1fa3
Oscarsa3 <oscarorlandosalinassilvestre@gmail.com>
2023-06-21 09:55:27 -0700

    feat: add a function that writes an Object to a text file, using a JSON representation

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""function that writes an Object to a text file, using a JSON
+representation"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """writes an object to a text file"""
+    with open(filename, 'w') as f:
+        return f.write(json.dumps(my_obj))

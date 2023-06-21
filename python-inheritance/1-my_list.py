c7d6282ad42f457578035c873ae65883198a807a
Oscarsa3 <oscarorlandosalinassilvestre@gmail.com>
2023-06-21 00:21:31 -0700

    fix: documentation

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""class MyList that inherits from list"""
+"""class MyList that inherits from list and has a public instance
+method that prints the list but sorted"""
 
 
 class MyList(list):
     """This class inherits from list, have a method"""
+
     def print_sorted(self):
         """Ordena de forma ascendente"""
+
         print(sorted(self))

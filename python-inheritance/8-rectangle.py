b90acaec2c6fd6090ec46b0e2130b69eb1100f35
Oscarsa3 <oscarorlandosalinassilvestre@gmail.com>
2023-06-20 22:43:24 -0700

    fix: documentation

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,7 +4,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """ """
+    """Rectangle that inherits from BaseGeometry"""
     def __init__(self, width, height):
         """Instantion with args width and height"""
         self.__width = width

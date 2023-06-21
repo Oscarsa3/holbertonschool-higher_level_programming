4942d4a9a520a183cadf7136abff5a0e6489b79a
Oscarsa3 <oscarorlandosalinassilvestre@gmail.com>
2023-06-20 23:47:57 -0700

    feat: class Square that inherits from Rectangle

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a class Square that inherits from Rectangle,
+size must be private and must be a positive integer and
+the area() must be implemented"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Implementation of the class Square"""
+
+    def __init__(self, size):
+        """Size must be private and positive integer"""
+        super().__init__(size, size)
+        self.__size = size
+        self.integer_validator("size", size)

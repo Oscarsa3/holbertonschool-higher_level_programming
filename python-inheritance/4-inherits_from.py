f23455deaec604013ad846e3c26fa1b28f43898c
Oscarsa3 <oscarorlandosalinassilvestre@gmail.com>
2023-06-21 16:47:53 -0700

    fix: change form

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,7 +6,7 @@ from the specified class ; otherwise False."""
 
 def inherits_from(obj, a_class):
     """Return True if obj is an instance of a clas"""
-    if issubclass(type(obj), a_class):
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     else:
         return False

76da7d3fd9ff290c3cca32a22412f0334f880eb3
Oscarsa3 <oscarorlandosalinassilvestre@gmail.com>
2023-06-21 00:14:59 -0700

    fix: add area method

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -12,9 +12,12 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """Size must be private and positive integer"""
-        super().__init__(size, size)
         self.__size = size
         self.integer_validator("size", size)
+
+    def area(self):
+        """Return the area"""
+        return self.__size * self.__size
 
     def __str__(self):
         """Return a function of the class"""

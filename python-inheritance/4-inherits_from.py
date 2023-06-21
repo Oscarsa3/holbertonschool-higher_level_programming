804365802adc9839594098d80c0e460748af32ba
Oscarsa3 <oscarorlandosalinassilvestre@gmail.com>
2023-06-21 16:50:28 -0700

    fix: change form

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """function that returns True if the object is an instance
-of a class that inherited (directly or indirectly)
-from the specified class ; otherwise False."""
+of a class, otherwise False"""
 
 
 def inherits_from(obj, a_class):

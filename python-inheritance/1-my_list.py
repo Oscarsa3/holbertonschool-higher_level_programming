a4d8bd56e7e0b2dfa7f20458e460fb3d1e415219
Oscarsa3 <oscarorlandosalinassilvestre@gmail.com>
2023-06-21 16:58:01 -0700

    fix: documentation

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""define class mylist that print sorted
-in ascending sort"""
+"""define class mylist that print sortedin ascending
+sort"""
 
 
 class MyList(list):
     """Print_sorted method"""
+
     def print_sorted(self):
         """Print a list sorted"""
+
         print(sorted(self))

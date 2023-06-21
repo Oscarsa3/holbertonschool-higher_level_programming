0615e3de73514b82562739f1a9a9d735f71ccd4e
Oscarsa3 <oscarorlandosalinassilvestre@gmail.com>
2023-06-21 09:48:00 -0700

    feat: add a function that returns an object (Python data structure) represented by a JSON string

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""function that returns an object (Python data structure)
+represented by a JSON string"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """return an object represented by a json string"""
+    return json.loads(my_str)

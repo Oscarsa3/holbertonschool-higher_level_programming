685df2f07d9b8a794fd66dfa34e410e7514ab31d
Oscarsa3 <oscarorlandosalinassilvestre@gmail.com>
2023-06-23 21:19:35 -0700

    feat: add a function  that returns a list of lists of integers representing the Pascal’s triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""function  that returns a list of lists of integers representing
+the Pascal’s triangle"""
+
+
+def pascal_triangle(n):
+    """Return a matrix of integers representing the Pascal's triangle of n"""
+    if n <= 0:
+        return [[]]
+
+    tri = [[1]]
+    for i in range(n - 1):
+        new = [1]
+        for x in range(len(tri) - 1):
+            new.append(tri[i][x] + tri[i][x + 1])
+        new.append(1)
+        tri.append(new)
+    return tri

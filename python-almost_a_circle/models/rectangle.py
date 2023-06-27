9d26e86b175d743c49b5d0d7d148cbbae0cbec62
Oscarsa3 <oscarsadev@gmail.com>
2023-06-26 17:13:33 -0700

    feat: changing the prototype to update(self, *args, **kwargs) that assigns a key/value argument to attributes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -80,15 +80,29 @@ class Rectangle(Base):
             print(" " * self.__x, end='')
             print("#" * self.__width)
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """Update arguments to each attributes"""
-        if args[0]:
-            super().__init__(args[0])
-        if len(args) == 2:
-            self.__width = args[1]
-        if len(args) == 3:
-            self.__height = args[2]
-        if len(args) == 4:
-            self.__x = args[3]
-        if len(args) == 5:
-            self.__y = args[4]
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if key == "id":
+                    super().__init__(value)
+                if key == "width":
+                    self.__width = value
+                if key == "height":
+                    self.__height = value
+                if key == "x":
+                    self.__x = value
+                if key == "y":
+                    self.__y = value
+
+        if args is not None:
+            if len(args) == 1:
+                super().__init__(args[0])
+            if len(args) == 2:
+                self.__width = args[1]
+            if len(args) == 3:
+                self.__height = args[2]
+            if len(args) == 4:
+                self.__x = args[3]
+            if len(args) == 5:
+                self.__y = args[4]

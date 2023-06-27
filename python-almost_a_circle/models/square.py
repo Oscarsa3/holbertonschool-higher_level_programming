e0ac7e22a1332b8233b03d037a033891742f5984
Oscarsa3 <oscarsadev@gmail.com>
2023-06-26 17:32:19 -0700

    feat: averride str method

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -10,3 +10,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Class constructor"""
         super().__init__(size, size, x, y)
+
+    def __str__(self):
+        return f"""[{self.__class__.__name__}] ({self.id}) \
+{self.x}/{self.y} - {self.width}"""

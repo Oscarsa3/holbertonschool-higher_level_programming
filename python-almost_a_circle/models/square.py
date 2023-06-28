16b52b437d75ec4748bac2c803ffe324bdd3870d
Oscarsa3 <oscarsadev@gmail.com>
2023-06-27 18:09:30 -0700

    feat: adding the public method that assigns attributes with *args and **kwargs

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -22,8 +22,42 @@ class Square(Rectangle):
         elif value <= 0:
             raise ValueError("width must be > 0")
         self.width = value
+        self.height = value
 
     def __str__(self):
         """Overrride str method"""
         return f"[{self.__class__.__name__}] ({self.id}) \
 {self.x}/{self.y} - {self.width}"
+
+    def update(self, *args, **kwargs):
+        """Update arguments to each attributes"""
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "size":
+                    self.width = value
+                    self.height = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
+
+        if args is not None:
+            if len(args) == 1:
+                self.id = args[0]
+            if len(args) == 2:
+                self.id = args[0]
+                self.width = args[1]
+                self.height = args[1]
+            if len(args) == 3:
+                self.id = args[0]
+                self.width = args[1]
+                self.height = args[1]
+                self.x = args[2]
+            if len(args) == 4:
+                self.id = args[0]
+                self.width = args[1]
+                self.height = args[1]
+                self.x = args[2]
+                self.y = args[3]

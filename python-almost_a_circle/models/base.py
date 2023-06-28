85f9c7a86a93fd23b17b2da2be908aeb8768a584
Oscarsa3 <oscarsadev@gmail.com>
2023-06-27 18:43:16 -0700

    feat: adding the static method that return the json string representation

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,9 @@
 """First class base"""
 
 
+import json
+
+
 class Base:
     """Our class base"""
     __nb_objects = 0
@@ -13,3 +16,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             Base.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """return the JSON string representation"""
+        if list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)

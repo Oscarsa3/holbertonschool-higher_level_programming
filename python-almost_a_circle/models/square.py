cd8bf01575e5a4f999ea671cf6ff6ce7eaee3e05
Oscarsa3 <oscarsadev@gmail.com>
2023-06-27 17:13:20 -0700

    fix: some errors

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -9,7 +9,7 @@ class Square(Rectangle):
     """New class Square"""
     def __init__(self, size, x=0, y=0, id=None):
         """Class constructor"""
-        super().__init__(size, size, x, y)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """Overrride str method"""

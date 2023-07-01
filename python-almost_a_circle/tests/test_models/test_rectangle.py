17fd51c759ecd3286875bc8777b2e51ad371ade1
Oscarsa3 <oscarsadev@gmail.com>
2023-06-30 17:52:53 -0700

    feat: more cases

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -9,6 +9,21 @@ from models.rectangle import Rectangle
 class Test_Rectangle(unittest.TestCase):
     """Test for differents cases"""
     def test_class_rectangle(self):
+
+        # r4 = Rectangle(19, 5)
+        # r4.width = -10
+        # self.assertRaises(ValueError, r4.width)
+        # r4.height = "2"
+        # self.assertRaises(TypeError, r4.height)
+        # r4.x = {}
+        # self.assertRaises(TypeError, r4.x)
+        # r4.y = -1
+        # self.assertRaises(ValueError, r4.width)
+
+        r2 = Rectangle(12, 8)
+        self.assertAlmostEqual(r2.height, 8)
+        self.assertAlmostEqual(r2.width, 12)
+
         r1 = Rectangle(10, 2, 0, 0, 12)
         self.assertAlmostEqual(r1.id, 12)
         self.assertAlmostEqual(r1.width, 10)
@@ -19,6 +34,6 @@ class Test_Rectangle(unittest.TestCase):
     def test_pep8_conformance(self):
         """Test that we conform to PEP8."""
         style = pycodestyle.StyleGuide(quiet=True)
-        result = style.check_files(['models/rectangle.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+        result = style.check_files(['models/rectangle.py',
+                                    'tests/test_models/test_rectangle.py'])
+        self.assertEqual(result.total_errors, 0, "Found errors")

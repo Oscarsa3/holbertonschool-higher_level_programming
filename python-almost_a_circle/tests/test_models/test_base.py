ac5f9f8c8dd34836d63ddf06cb8ca8bb923f411c
Oscarsa3 <oscarsadev@gmail.com>
2023-07-01 17:55:13 -0700

    feat: more test cases

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Unittest for class base
 """
+
+
 import unittest
 import pycodestyle
 import json
@@ -25,16 +27,16 @@ class Test_Base(unittest.TestCase):
         self.assertAlmostEqual(b6.id, 4)
 
     def test_to_json_string(self):
-        """other method"""
-        self.assertAlmostEqual(Base.to_json_string([]), [])
-        self.assertAlmostEqual(Base.to_json_string(None), [])
+        """test for this method"""
+        self.assertAlmostEqual(Base.to_json_string([]), "[]")
+        self.assertAlmostEqual(Base.to_json_string(None), "[]")
         js = json.dumps([{'width': 4, 'height': 5}])
         self.assertAlmostEqual(Base.to_json_string([{'width': 4,
                                                    'height': 5}]), js)
         self.assertAlmostEqual(str, type(js))
 
     def test_from_json_strin(self):
-        """other method"""
+        """test for this method"""
         self.assertAlmostEqual(Base.from_json_string(""), [])
         self.assertAlmostEqual(Base.from_json_string(None), [])
         js = json.loads('[{"width": 4, "height": 5}]')
@@ -42,8 +44,24 @@ class Test_Base(unittest.TestCase):
                                                    "height": 5}]'), js)
         self.assertAlmostEqual(list, type(js))
 
+    def test_save_to_file(self):
+        """Test for this method"""
+        Rectangle.save_to_file(None)
+        with open("Rectangle.json", "r") as f:
+            self.assertAlmostEqual(f.readline(), '[]')
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+
+        Rectangle.save_to_file([r1, r2])
+        fi1 = r1.to_dictionary()
+        fi2 = r2.to_dictionary()
+        filec = [fi1, fi2]
+        fi3 = Rectangle.to_json_string(filec)
+        with open("Rectangle.json", "r") as f:
+            self.assertAlmostEqual(f.readline(), fi3)
+
     def test_pycodestyle_conformance(self):
         """Test that we conform to PEP8."""
         style = pycodestyle.StyleGuide(quiet=True)
-        result = style.check_files(['models/base.py'])
+        result = style.check_files(['models/base.py', 'models/rectangle.py'])
         self.assertEqual(result.total_errors, 0, "Found errors")

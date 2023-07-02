c485829dfcc3dac145df8d94910a4c6bdc714076
Oscarsa3 <oscarsadev@gmail.com>
2023-07-01 17:53:33 -0700

    fix: when is None return a string "[]"

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -21,7 +21,7 @@ class Base:
     def to_json_string(list_dictionaries):
         """return the JSON string representation"""
         if list_dictionaries is None or not list_dictionaries:
-            return []
+            return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod

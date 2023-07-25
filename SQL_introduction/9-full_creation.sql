ada593ba0b9ed372a2fce61fe6e4168064e16827
Oscarsa3 <oscarorlandosalinassilvestre@gmail.com>
2023-07-24 22:36:02 -0700

    change the form

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,4 +1,6 @@
 -- creates a table second_table in the database hbtn_0c_0 and add multiples rows.
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT, PRIMARY KEY (id));
-INSERT IGNORE INTO second_table (id, name, score)
-VALUES (1, 'Jhon', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) VALUES (1, 'Jhon', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);

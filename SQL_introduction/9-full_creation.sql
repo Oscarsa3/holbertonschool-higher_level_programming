9c644a887f006a45395a54b343388b6c8b565b8c
Oscarsa3 <oscarorlandosalinassilvestre@gmail.com>
2023-07-24 22:04:46 -0700

    fixing some error

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
 -- Script that creates a table in the database
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 
-INSERT INTO second_table (id, name, score) VALUES (1, 'Jhon', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
+INSERT INTO second_table VALUES (1, 'Jhon', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);

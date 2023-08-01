27354e9684294022f5eed975025c544530f6d7d1
Oscarsa3 <oscarorlandosalinassilvestre@gmail.com>
2023-07-31 17:25:39 -0700

    fixing typing query

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect("localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id")
+    cur.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id;")
     rows = cur.fetchall()
     for row in rows:
         print(row)

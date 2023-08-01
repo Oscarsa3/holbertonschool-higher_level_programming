6a56b74b8be5a90d16a12e38fa514ec31673604a
Oscarsa3 <oscarorlandosalinassilvestre@gmail.com>
2023-07-31 20:11:11 -0700

    adding a requirement

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -11,10 +11,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect("localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute(f"""SELECT id, name
+    cur.execute("""SELECT id, name
                 FROM states
-                WHERE name = '{sys.argv[4]}'
-                ORDER BY id""")
+                WHERE name = '{}'
+                ORDER BY id""".format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)

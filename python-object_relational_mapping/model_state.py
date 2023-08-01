3b5777b845a4d70ec34950c300f120484f1031a3
Oscarsa3 <oscarorlandosalinassilvestre@gmail.com>
2023-08-01 09:32:14 -0700

    adding requirements

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,9 +1,11 @@
 #!/bin/usr/python3
 """contains the class definition of a State and an
 instance Base = declarative_base()"""
-import sqlalchemy
+
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+
 
 Base = declarative_base()
 
@@ -11,5 +13,5 @@ Base = declarative_base()
 class State(Base):
     """This class has a id"""
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)

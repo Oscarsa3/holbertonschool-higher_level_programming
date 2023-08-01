8acc91f19a86c5e0715245dd4409cc4410f0a9c7
Oscarsa3 <oscarorlandosalinassilvestre@gmail.com>
2023-08-01 10:45:21 -0700

    adding script that print only the frist state object from tha database

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""print only the first State objects from the database hbtn_0e_6_usa
+with the module SQLAlchemy"""
+
+
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+
+if __name__ == "__main__":
+    engine = create_engine(f"""mysql+mysqldb://{sys.argv[1]}:
+                           {sys.argv[2]}@localhost:3306/{sys.argv[3]}""",
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    sesion = Session()
+
+    rows = sesion.query(State).order_by(State.id).first()
+    print(f"{rows.id}:", rows.name)
+
+    sesion.close()

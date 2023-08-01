48e13b18eff09925eb73a9244b94e1f3a58654d4
Oscarsa3 <oscarorlandosalinassilvestre@gmail.com>
2023-07-31 20:16:05 -0700

    adding BINARY in the QUERY

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("""SELECT id, name
                 FROM states
-                WHERE name = '{}'
+                WHERE BINARY name = '{}'
                 ORDER BY id""".format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:

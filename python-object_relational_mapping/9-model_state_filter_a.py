f0130e44924df85ac424a84d426093514c2d39c4
Oscarsa3 <oscarorlandosalinassilvestre@gmail.com>
2023-08-01 17:25:41 -0700

    adding script taht lists all state objects that contain the letter a

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""lists all State objects that contain the letter a
+from the database hbtn_0e_6_usa with the module SQLAlchemy"""
+
+
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+
+if __name__ == "__main__":
+    engine = create_engine(f"""mysql+mysqldb://{sys.argv[1]}:
+                           {sys.argv[2]}@localhost:3306/{sys.argv[3]}""",
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    sesion = Session()
+
+    rows = sesion.query(State
+                        ).filter(State.name.like('%a%')
+                                 ).order_by(State.id
+                                            ).all()
+    for row in rows:
+        print(f"{row.id}:", row.name)
+
+    sesion.close()

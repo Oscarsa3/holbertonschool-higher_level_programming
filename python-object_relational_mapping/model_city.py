c503c03b5b35cebabbf43b9f949d288210ebcd46
Oscarsa3 <oscarorlandosalinassilvestre@gmail.com>
2023-08-01 19:34:39 -0700

    adding Python file that contain the class definiton of the cities

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""contains the class definition of a City"""
+
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from model_state import Base, State
+
+
+class City(Base):
+    """This class has a id"""
+
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+
+    state = relationship('State')
